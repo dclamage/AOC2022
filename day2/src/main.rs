b5ec15558d6900ab9f430755472f21cee71348a1
David Clamage <dclamage@users.noreply.github.com>
2022-12-01 22:39:45 -0800

    Day 2 parts 1 and 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,5 +1,5 @@
-use std::time::*;
 use std::io::{self, Write};
+use std::time::*;
 use utility::*;
 
 fn main() {
@@ -10,9 +10,10 @@ fn main() {
     writeln!(stdout, "Parsing...").unwrap();
     let start_time = Instant::now();
     let file_lines = read_file_lines("day2/input.txt");
+    //let file_lines = read_file_lines("day2/example_input.txt");
     let elapsed = start_time.elapsed();
     writeln!(stdout, "Parsing time: {}us\n", elapsed.as_micros()).unwrap();
-    
+
     // Part 1
     writeln!(stdout, "*********** PART 1 ***********").unwrap();
     let start_time = Instant::now();
@@ -30,10 +31,104 @@ fn main() {
     writeln!(stdout, "Part 2 time: {}us", elapsed.as_micros()).unwrap();
 }
 
+#[derive(Copy, Clone, PartialEq)]
+enum RPSMove {
+    Rock,
+    Paper,
+    Scissors,
+}
+
+fn beats(move_a: RPSMove, move_b: RPSMove) -> bool {
+    match (move_a, move_b) {
+        (RPSMove::Rock, RPSMove::Scissors) => true,
+        (RPSMove::Paper, RPSMove::Rock) => true,
+        (RPSMove::Scissors, RPSMove::Paper) => true,
+        _ => false,
+    }
+}
+
+fn move_score(m: RPSMove) -> u32 {
+    match m {
+        RPSMove::Rock => 1,
+        RPSMove::Paper => 2,
+        RPSMove::Scissors => 3,
+    }
+}
+
 fn part1(file_lines: &Vec<String>) -> String {
-    file_lines[0].clone()
+    let mut score = 0;
+    for line in file_lines.iter() {
+        let parts = line.split_ascii_whitespace().collect::<Vec<&str>>();
+        let enemy_move = parts[0];
+        let my_move = parts[1];
+        let enemy_move = match enemy_move {
+            "A" => RPSMove::Rock,
+            "B" => RPSMove::Paper,
+            "C" => RPSMove::Scissors,
+            _ => panic!("Invalid enemy move"),
+        };
+        let my_move = match my_move {
+            "X" => RPSMove::Rock,
+            "Y" => RPSMove::Paper,
+            "Z" => RPSMove::Scissors,
+            _ => panic!("Invalid my move"),
+        };
+        score += move_score(my_move)
+            + if enemy_move == my_move {
+                3
+            } else if beats(my_move, enemy_move) {
+                6
+            } else {
+                0
+            };
+    }
+
+    format!("{}", score)
+}
+
+fn winning_move(m: RPSMove) -> RPSMove {
+    match m {
+        RPSMove::Rock => RPSMove::Paper,
+        RPSMove::Paper => RPSMove::Scissors,
+        RPSMove::Scissors => RPSMove::Rock,
+    }
+}
+
+fn losing_move(m: RPSMove) -> RPSMove {
+    match m {
+        RPSMove::Rock => RPSMove::Scissors,
+        RPSMove::Paper => RPSMove::Rock,
+        RPSMove::Scissors => RPSMove::Paper,
+    }
 }
 
 fn part2(file_lines: &Vec<String>) -> String {
-    file_lines[0].clone()
+    let mut score = 0;
+    for line in file_lines.iter() {
+        let parts = line.split_ascii_whitespace().collect::<Vec<&str>>();
+        let enemy_move = parts[0];
+        let my_move = parts[1];
+        let enemy_move = match enemy_move {
+            "A" => RPSMove::Rock,
+            "B" => RPSMove::Paper,
+            "C" => RPSMove::Scissors,
+            _ => panic!("Invalid enemy move"),
+        };
+        let my_move = match my_move {
+            "X" => losing_move(enemy_move),
+            "Y" => enemy_move,
+            "Z" => winning_move(enemy_move),
+            _ => panic!("Invalid my move"),
+        };
+        score += move_score(my_move)
+            + if enemy_move == my_move {
+                3
+            } else if beats(my_move, enemy_move) {
+                6
+            } else {
+                0
+            };
+    }
+
+    format!("{}", score)
 }
