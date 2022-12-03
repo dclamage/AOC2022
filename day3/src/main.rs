0d29b4b651f737b23efb6a235174292a49c6e5dc
David Clamage <dclamage@users.noreply.github.com>
2022-12-02 23:31:52 -0800

    Day 3

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -31,10 +31,42 @@ fn main() {
     writeln!(stdout, "Part 2 time: {}us", elapsed.as_micros()).unwrap();
 }
 
+fn priority(a: char) -> i64 {
+    if a.is_ascii_lowercase() {
+        a as i64 - 'a' as i64 + 1
+    } else {
+        a as i64 - 'A' as i64 + 27
+    }
+}
+
+fn str_bits(s: &str) -> u64 {
+    s.chars().fold(0, |acc, c| acc | 1 << priority(c))
+}
+
 fn part1(file_lines: &Vec<String>) -> String {
-    "".to_owned()
+    let mut priority_total: i64 = 0;
+    for line in file_lines.iter() {
+        let (left, right) = line.split_at(line.len() / 2);
+        let common_bits = str_bits(left) & str_bits(right);
+        assert!(common_bits != 0 && common_bits.count_ones() == 1);
+        priority_total += common_bits.trailing_zeros() as i64;
+    }
+
+    format!("{}", priority_total)
 }
 
 fn part2(file_lines: &Vec<String>) -> String {
-    "".to_owned()
+    let mut priority_total: i64 = 0;
+    let mut common_line_bits = u64::MAX;
+    for (line_index, line) in file_lines.iter().enumerate() {
+        common_line_bits &= str_bits(line);
+
+        if line_index % 3 == 2 {
+            assert!(common_line_bits != 0 && common_line_bits.count_ones() == 1);
+            priority_total += common_line_bits.trailing_zeros() as i64;
+            common_line_bits = u64::MAX;
+        }
+    }
+
+    format!("{}", priority_total)
 }
